ad74b2d3220e52faf86555346eeddd15451ebd91
Zafir Hasan <zafirhsn@gmail.com>
2018-11-01 12:47:46 -0400

    Working on design

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class Node {
+// int state[3][3];
+// Node* parent;
+// Node* child;
+// int heuristic;
+// char action;
+
+};
+
+void graphSearchA(std::string file) {
+	// vector<char> path;
+	// int initialState[3][3];
+	// int goalState[3][3];
+	// std::vector<Node*> frontier;
+	// std::vector<Node*> explored; 
+	// 
+
+}
+
+int main() {
+	std::cout << "Hello, world" << std::endl;
+}
