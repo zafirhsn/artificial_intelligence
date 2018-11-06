1da07f52d52f082d719e4b9ba636b54b42691e29
Zafir Hasan <zafirhsn@gmail.com>
2018-11-05 23:28:27 -0500

    Reimplement priority queue with vector

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@
 
 using namespace std;
 
+// The node class will contain member variables that contain the state of the node, the path-cost up to that node from the root node, and the path up to that node from the root node.  
 class Node {
 public:
 	Node(vector<vector<int>> state) : state(state), pathCost(0), path({}) {}
-	Node(vector<vector<int>> state, int pathCost, vector<string> path) : state(state), pathCost(pathCost), path(path) {}
+	Node(vector<vector<int>> state, int pathCost, vector<char> path) : state(state), pathCost(pathCost), path(path) {}
 
 	void displayState() const {
 		string stringstate;
@@ -40,29 +41,59 @@ public:
 		return pathCost;
 	}
 
-	vector<string> getPath() const {
+	vector<char> getPath() const {
 		return path;
+	}
+
+	vector<vector<int>> getState() const {
+		return state;
 	}
 
 private: 
 	vector<vector<int>> state;
 	int pathCost;
-	vector<string> path;
+	vector<char> path;
 
 };
 
+// This function defines the comparison operator that is used in a min-heap with the user defined type, node. 
 class cmpFunction {
 	public:
 		int operator()(const Node& a, const Node& b) {
 			return (a.getPathCost() > b.getPathCost());
 		}
-
 };
 
-void solution(const Node& node) {
-
+// This function creates the output file and writes to the file in the format specified in the project problem once the solution node has been found
+void solution(const Node& node, const vector<vector<int>>& initialState, const vector<vector<int>>& goalState) {
+	ofstream outputFile;
+	outputFile.open("Output1.txt");
+	string fstring;
+	for (size_t i = 0; i < initialState.size(); i++) {
+		for (size_t j = 0; j < initialState[i].size(); j++) {
+			fstring += to_string(initialState[i][j]) + " ";
+		}
+		fstring += '\n';
+	}
+	fstring += '\n';
+	for (size_t i = 0; i < goalState.size(); i++) {
+		for (size_t j = 0; j < goalState[i].size(); j++) {
+			fstring += to_string(goalState[i][j]) + " ";
+		}
+		fstring += '\n';
+	}
+	fstring += '\n';
+	vector<char> path = node.getPath();
+	fstring += to_string(path.size()) + '\n';
+	fstring += "N\n";
+	for (size_t i = 0; i < path.size(); i++) {
+			fstring += path[i] + " ";
+	}
+	outputFile << fstring;
+	outputFile.close();
 }
 
+// This function returns the manhattan distance of one tile from its goal position
 int tileDist(int tile, const vector<vector<int>>& currState, const vector<vector<int>>& goalState) {
 	
 	vector<int> stateCoor;
@@ -83,11 +114,12 @@ int tileDist(int tile, const vector<vector<int>>& currState, const vector<vector
 	int xdiff = abs(stateCoor[0] - goalCoor[0]);
 	int ydiff = abs(stateCoor[1] - goalCoor[1]);
 
-	int tileDist = max(xdiff, ydiff);
+	int tileDist = xdiff + ydiff;
 	return tileDist;
 
 }
 
+// This function returns the sum of Manhattan distances of all tiles (except blank) from their goal position
 int heuristic(const vector<vector<int>>& currState, const vector<vector<int>>& goalState) {
 	int sumDist = 0;
 	for (int i = 1; i < 9; i++) {
@@ -96,14 +128,16 @@ int heuristic(const vector<vector<int>>& currState, const vector<vector<int>>& g
 	return sumDist;
 }
 
+// This function returns true if the state is equivalent to the goal state. It is equivalent when the sum of Manhattan distances of the tiles from their goal position is equal to 0. It returns false otherwise.
 bool goalTest(const vector<vector<int>>& state, const vector<vector<int>>& goal) {
 	return heuristic(state, goal) == 0;
 }
 
+// This function returns a vector containing all possible actions doable from a given state
 vector<char> actions(const vector<vector<int>>& state) {
-	enum position {
-		TOP_RIGHT
-	}
+	// enum position {
+	// 	TOP_RIGHT
+	// }
 
 	vector<char> vecActions;
 	vector<int> coors;
@@ -118,43 +152,72 @@ vector<char> actions(const vector<vector<int>>& state) {
 
 	if (coors[0] == 0) {
 		vecActions.push_back('D');
-		if (coors[1] == 0) {
-			vecActions.push_back('R');
-		}
-		else if (coors[1] == 1) {
-			vecActions.push_back('R');
-			vecActions.push_back('L');
-		}
-		else if (coors[1] == 2) {
-			vecActions.push_back('L');
-			vecActions.push_back('D');
-		}
 	}
 	else if (coors[0] == 1) {
 		vecActions.push_back('D');
 		vecActions.push_back('U');
-		if (coors[1] == 1) {
-			vecActions.push_back('L');
-			vecActions.push_back('R');
-		}
-		else if (coors[1] == 0) {
-			vecActions.push_back('R');
-		}
-		else {
-			vecActions.push_back('L');
-		}
 	}
 	else {
 		vecActions.push_back('U');
 	}
-	
 
+		
+	if (coors[1] == 0) {
+		vecActions.push_back('R');
+	}
+	else if (coors[1] == 1) {
+		vecActions.push_back('R');
+		vecActions.push_back('L');
+	}
+	else {
+		vecActions.push_back('L');
+	}
+
+	return vecActions;	
 }
 
-Node child-node(const Node& currNode, const char action) {
 
+// This function creates a child node using the state of the current node, a legal action, and the goal state for the problem
+Node* childNode(const Node& currNode, const char action, const vector<vector<int>>& goalState) {
+	vector<vector<int>> childState = currNode.getState();
+
+	vector<char> path = currNode.getPath(); 
+	path.push_back(action);
+
+	int fcost = currNode.getPathCost() - heuristic(currNode.getState(), goalState) + 1;
+
+	int temp;
+	for (size_t i = 0; i < childState.size(); i++) {
+		for (size_t j = 0; j < childState[i].size(); j++) {
+			if (childState[i][j] == 0) {
+				if (action == 'U') {
+					temp = childState[i-1][j];
+					childState[i-1][j] = 0;
+				}
+				else if (action == 'D') {
+					temp = childState[i+1][j];
+					childState[i+1][j] = 0;
+				}
+				else if (action == 'L') {
+					temp = childState[i][j-1];
+					childState[i][j-1] = 0;
+				}
+				else {
+					temp = childState[i][j+1];
+					childState[i][j+1] = 0;
+				}
+				childState[i][j] = temp;
+			}
+		}
+	}
+
+	int pathCost = heuristic(childState, goalState) + fcost;
+	Node* child = new Node(childState, pathCost, path);
+	return child;
 }
 
+
+// GRAPH-SEARCH A* using Sum of Manhattan distances of tiles from their goal position as heuristic. 
 void graphSearchA(string file) {
 	ifstream ifs(file);
 	if (!ifs) {
@@ -169,16 +232,12 @@ void graphSearchA(string file) {
 	int counter = 0;
 	vector< vector<int> > initialState(3);
 	vector< vector<int> > goalState(3);
-
-	// cout << initialState[3][2] << endl;
 	
 	int i = 0;
 	int j = 0;
 	while (ifs >> tile) {
 		if (i < 3) {
 			initialState[i].push_back(tile);
-			// cout << i << endl;
-			// cout << tile << endl;
 		}
 		else {
 			goalState[i-3].push_back(tile);
@@ -190,88 +249,100 @@ void graphSearchA(string file) {
 	}
 	ifs.close();
 
-	// cout << "The tile distance for 2 is: " << 
-	int chessDest = tileDist(2, initialState, goalState); 
-	// << endl: 
-	cout << "The tile distance for 2 is: " << chessDest << endl;
 
+	// Node class TESTS
+	cout << "Node class tests: " << endl;
+	Node test(initialState, heuristic(initialState, goalState), {});
+	test.displayState();
+	test.displayPath();
+	test.displayPathCost();
+
+	// tileDist TESTS
+	int manDist2 = tileDist(3, initialState, goalState); 
+	cout << "The tile distance for 3 is: " << manDist2 << endl;
+	int manDist8 = tileDist(8, initialState, goalState);
+	cout << "The tile distance for 8 is: " << manDist8 << endl;
+
+	// heuristic TESTS
 	int sumDist = heuristic(initialState, goalState);
-	cout << "The sum of all chessboard distances is: " << sumDist << endl;
+	cout << "The sum of all Manhattan distances is: " << sumDist << endl;
 
-	Node root(initialState);
-	root.displayState();
-	root.displayPath();
-	root.displayPathCost();
+	// goaltest TESTS
+	cout << "Goal Test" << endl;
+	cout << goalTest(initialState, goalState) << endl;
 
-	Node goal(goalState, -6, {});
+	// actions TESTS 
+	vector<char> vecActions = actions(test.getState());
+	cout << "These are the actions for this state" << endl;	 
+	for (size_t i = 0; i < vecActions.size(); i++) {
+		cout << vecActions[i] << endl;
+	}
+
+	// childNode TESTS
+	cout << "Child node test: " << endl;
+	Node* child = childNode(test, 'U', goalState);
+	child->displayState();
+	child->displayPathCost();
+	child->displayPath();
+
+	// frontier TESTS
+	cout << "Frontier test: " << endl;
+	// Node goal(goalState, 6, {});
+	// priority_queue<Node, vector<Node>, cmpFunction > frontier;
+	// vector<Node*> explored;
+
+	// frontier.push(root);
+	// frontier.push(goal);
+
+	// Node n = frontier.top();
+	// n.displayState();
+	// cout << frontier.top() << endl;
 
 
+	// soultion TESTS
+	cout << "This is a solution test: " << endl;
+	solution(test, initialState, goalState);
+
+	cout << "======================" << endl;
+	cout << "======================" << endl;
+	cout << endl;
+
+	Node root(initialState, heuristic(initialState, goalState), {});
+	priority_queue<Node, vector<Node>, cmpFunction > frontier;
+	frontier.push(root);
+	vector<Node*> explored;
 
 
-	// string line;
-	// while (getline(ifs, line)) {
-	// 	for (size_t i = 0; i < line.length(); i++) {
-	// 		if (line[i] != '\0' && line[i] != ' ' && line[i] != '\n') {
-	// 			initialState
+	
+	// while(true) {
+	// 	if (frontier.empty()) { 
+	// 		cerr << "Search has failed" << endl;
+	// 		exit(1);
+	// 	}
+	// 	Node n = frontier.pop();
+	// 	if (goalTest(n.getState(), goalState)) {
+	// 		// return solution(n);
+	// 	}
+	// 	explored.push_back(n);
+	// 	vector<char> possActions = actions(n.getState());
+	// 	for (size_t i = 0; i < possActions.size(); i++)) {
+	// 		Node* nodeptr = childNode(n, possActions[i], goalState);
+	// 		if (nodeptr->getState() ) {
+
 	// 		}
 	// 	}
 	// }
 
-	priority_queue<Node, vector<Node>, cmpFunction > frontier;
-	vector<Node*> explored;
 
-	frontier.push(root);
-	frontier.push(goal);
+	
 
-	Node n = frontier.top();
-	n.displayState();
-	// cout << frontier.top() << endl;
-
-
-
-	// vector<char> path;
-	// int initialState[3][3];
-	// int goalState[3][3];
-	// vector<Node*> frontier;
-	// vector<Node*> explored; 
-	// 
 
 }
 
 int main() {
 	cout << "Hello, world" << endl;
 
-	// int state[3][3];
-	// state[0][2] = 100;
-	// cout << sizeof(state) / sizeof(*state) << endl;
-	// cout << *(state)[2] << endl;
-	// cout << endl;
-
-	// int test[10];
-	
-	// test[0] = 5;
-	// test[9] = 40;
-	// test[4] = 100;
-	// //  print test[0] = 5
-	// cout << *test << endl;
-	// //  print test[9] 
-	// cout << *(test+9) << endl;
-	// //  print test[4]
-
-
 	graphSearchA("Input1.txt");
-
-	// vector< vector<int> > multivec = {{1,2,3}, {4, 5, 6}, {7, 8, 9}};
-	// vector<string> path(1);
-	
-	// Node root(multivec, 0, path); 
-	// root.displayState();
-	// root.displayPathCost(); 
-	// root.displayPath();
-
-
-	// vector<int> vec = {2, 3, 4, 5, 6, 7};
-	// cout << vec[4] << endl;
 
 
 }
