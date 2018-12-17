1255d7fa692f01e2678f2677492d944761b68c53
Zafir Hasan <zafirhsn@gmail.com>
2018-12-16 23:58:57 -0500

    Working on individual neuron output

--- neural-network/main.cpp
+++ neural-network/main.cpp
@@ -36,45 +36,60 @@ float activation(float x) {
   return (1 / (1 + exp(-x)));
 }
 
-float nodeOutput(const vector<float>& input, const vector<float>& weights) {
+float neuronOutput(const vector<float>& input, const vector<float>& weights) {
   float weightSum = 0;
-  for (size_t i = 0; i < input.size(); i++) {
-    weightSum += input[i] * weights[i];
+  for (size_t i = 0; i <= input.size(); i++) {
+    if (i == input.size()) {
+      weightSum += -1 * weights[i];
+    }
+    else {
+      weightSum += input[i] * weights[i];
+    }  
   }
+
   cout << "Weighted sum of inputs: " << weightSum << endl;
   float output = activation(weightSum);
-  cout << "Output: " << output << endl;
+  cout << "Neuron Output: " << output << endl;
   return output;
+}
+
+void assert(const string& desc, float correctOut, float output) {
+  cout << desc << " SHOULD BE: " << correctOut << endl;
+  cout << desc << " IS : " << output << endl;
+  correctOut = (int)(correctOut / 0.0001) * 0.0001; 
+  output = (int)(output / 0.0001) * 0.0001; 
+  
+  if (correctOut == output) {
+    cout << "TEST PASSED" << endl;
+  }
+  else {
+    cout << "TEST FAILED" << endl;
+  }
 }
 
 void neuralNetwork(int numHidden) {
   vector<float> inputLayer(748, 0.002);
-  vector<vector<float>> hidWeights(100, vector<float>(748,0.5));
+  vector<vector<float>> hidWeights(100, vector<float>(749, 0.2));
   vector<float> hiddenLayer(numHidden);
-  vector<vector<float>> outWeights(5, vector<float>(100));
+  vector<vector<float>> outWeights(5, vector<float>(101,0.5));
   vector<float> outputLayer(5);
 
   outputLayer.push_back(5);
-  // initializeWeights(hidWeights);
+  initializeWeights(hidWeights);
   initializeWeights(outWeights);
 
-  float output = nodeOutput(inputLayer, hidWeights[0]);
-
-  cout << output << endl;
+  float output = neuronOutput(inputLayer, hidWeights[0]);
+  // assert("Hidden neuron 0", 0.248, output);
+  // assert("Hidden neuron 0 output" , 0.52478, output);
+  
 
 
 }
 
 
 int main() {
-    cout << "Hello world" << endl;
 
     neuralNetwork(100);
-
-    cout << activation(0.5) << endl;
-
-
-
 
 
 }
