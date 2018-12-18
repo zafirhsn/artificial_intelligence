edd6a0c794fc22a524504e40aab84507020e747c
Zafir Hasan <zafirhsn@gmail.com>
2018-12-17 19:26:04 -0500

    Pseudocode most of network

--- neural-network/main.cpp
+++ neural-network/main.cpp
@@ -36,6 +36,10 @@ float activation(float x) {
   return (1 / (1 + exp(-x)));
 }
 
+float derivSigmoid(float x) {
+  return (exp(-x) / pow((1 + exp(-x)), 2));
+}
+
 float neuronOutput(const vector<float>& input, const vector<float>& weights) {
   float weightSum = 0;
   for (size_t i = 0; i <= input.size(); i++) {
@@ -46,6 +50,7 @@ float neuronOutput(const vector<float>& input, const vector<float>& weights) {
       weightSum += input[i] * weights[i];
     }  
   }
+  // float weightSum = weightedSum(input, weights);
 
   cout << "Weighted sum of inputs: " << weightSum << endl;
   float output = activation(weightSum);
@@ -67,7 +72,55 @@ void assert(const string& desc, float correctOut, float output) {
   }
 }
 
+/*
+  float weightedSum(const vector<float>& input, const vector<float>& weights) {
+    float weightSum = 0;
+    for (size_t i = 0; i <= input.size(); i++) {
+      if (i == input.size()) {
+        weightSum += -1 * weights[i];
+      }
+      else {
+        weightSum += input[i] * weights[i];
+      }  
+    }
+    return weightSum;
+ }
+
+ updateWeights(weightLayer, learningRate, deltas) {
+ 
+ }
+*/
+
+/*
+void backPropagate(all the layers and weights) {
+  vector<float> outputDeltas(5);
+  vector<float> hiddenDeltas(100);
+  
+  for (size of output layer) {
+    float err = ideal  - outputLayer[i];
+    float weightSum = weightedSum(hiddenLayer, outWeights[i]);
+    float delta = err * derivSigmoid(weightSum);
+    outputDelta[i] = delta;
+  }
+  
+  updateWeights(weightLayer, learningRate, hiddenLayer, deltas);
+
+  for (size of hidden layer) {  
+    float delta = derivSigmoid(weightedSum(hiddenLayer[i])) * sumofAll (( weight of W(ji) * delta for neuron i ))
+    hiddenDeltas[i] = delta;
+  }
+
+  updateWeights(weightLayer, learningRate, inputLayer, deltas);
+  
+
+}
+
+*/
+
 void neuralNetwork(int numHidden) {
+  const float learningRate = 0.2;
+  float err = 1.0;
+
   vector<float> inputLayer(748, 0.002);
   vector<vector<float>> hidWeights(100, vector<float>(749, 0.2));
   vector<float> hiddenLayer(numHidden);
@@ -82,7 +135,18 @@ void neuralNetwork(int numHidden) {
   // assert("Hidden neuron 0", 0.248, output);
   // assert("Hidden neuron 0 output" , 0.52478, output);
   
+  /*
+  initializeActualOutputs
+  initializeInputs(file);
+  while (err > 2%) {
+    computeNodes(hiddenLayer, hiddenWeights, inputLayer);
+    computeNodes(outputLayer, outWeights, hiddenLayer);
+    backPropagate(outputLayer, outWeights, hiddenLayer, hidWeights, inputLayer, vector of actual ANSWER);
 
+
+  }
+
+  */
 
 }
 
@@ -90,6 +154,6 @@ void neuralNetwork(int numHidden) {
 int main() {
 
     neuralNetwork(100);
-
-
+    float x = derivSigmoid(0.67);
+    cout << x << endl;
 }
